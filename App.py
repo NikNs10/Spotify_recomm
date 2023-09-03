7431abed37d3ca3b90c5192304e71582d627824a
NikNs10 <nikhilsharma.ns503@gmail.com>
2023-09-03 20:47:32 +0530

    Added scaling and visualizations

--- App.py
+++ App.py
@@ -6,27 +6,24 @@ from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
 import pandas as pd
 import seaborn as sns
+from sklearn import preprocessing
+from scipy.spatial import distance
 
 #CSV_Data Loaded
-data=pd.read_csv("C:\Users\Ssharma\Documents\Project\Spotify_recomm\dataset.csv")
+data=pd.read_csv("dataset.csv")
 
 #To print first 5 rows of the data
 print(data.head())
 
 
-#Data Preprocessing
 
-#dropping null  and duplicate values
+
+#Dropping Null  and Duplicate Values
 df = data.drop("Unnamed: 0", axis=1)
 df.isna().sum()
 df = df.dropna()
 df.isna().sum()
 df = df.drop_duplicates()
-
-df.info()
-df.shape
-popular_artists = df.groupby("artists").count().sort_values(by='popularity', ascending=False)['popularity'][:5]
-popular_artists
 
 #Data Visualization
 
@@ -35,6 +32,31 @@ plt.figure(figsize=(16,4))
 sns.heatmap(corr_table, annot=True, fmt=".1g")
 plt.title("Correlation Heatmap between variables")
 plt.show()
+
+df.info()
+df.shape
+popular_artists = df.groupby("artists").count().sort_values(by='popularity', ascending=False)['popularity'][:5]
+popular_artists
+
+data_sort = data.drop(['time_signature', 'Unnamed: 0', 'key'], axis=1)
+data_sort.drop_duplicates(subset=['track_id'], inplace=True)
+
+#Data Preprocessing
+scaler = preprocessing.MinMaxScaler()
+names = data_sort.select_dtypes(include = np.number).columns
+d = scaler.fit_transform(data_sort.select_dtypes(include = np.number))
+data_norm = pd.DataFrame(d, columns=names)
+data_norm.set_index(data_sort.loc[:, 'track_id'], inplace=True)
+
+x = list(data_norm.iloc[19])
+
+
+data_result = pd.DataFrame()
+data_result['euclidean'] = [distance.euclidean(obj, x) for index, obj in data_norm.iterrows()]
+data_result['track_id'] = data_norm.index
+
+
+
 
 
 
